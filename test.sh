11d023bd5f4c5598138a30c9c22594c55392b6ab
shinshihe <56946695+shinshihe@users.noreply.github.com>
2021-09-22 16:22:13 +1000

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,21 +1,13 @@
-#!/bin/dash
-#cd .girt
-#cat index.txt
-#cd - 
-#cat a.txt
-
-#a=1
-#a=$(( $a + 1))
-#echo $a
-
-#a=$( diff a.txt ab.txt)
-#b=$( diff a.txt abc.txt)
-#echo ${#a}
-#echo ${#b}
-
-a=$(cat log.txt)
-echo $a
-for i in $a
+#!/bin/bash
+name=()
+for i in *
 do
-	echo $i
+	echo "$i"
+	name+=("$i")
 done
+
+for i in ${name[@]}
+do
+	echo "$i"
+done
+
